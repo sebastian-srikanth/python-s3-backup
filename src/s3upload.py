96e8416b3a5e7b540a3ba46e05274084c2d412b2
Sebastian <Sebastin.X.Kumar.-ND@disney.com>
2021-11-28 17:32:59 +0000

    updated python script added config yaml

--- src/s3upload.py
+++ src/s3upload.py
@@ -1,49 +1,67 @@
-import boto3
 import os
+import boto3
+import yaml
 
-
-s3_resource = boto3.resource(
-    's3',
-     aws_access_key_id='', 
-     aws_secret_access_key='', 
-)
-
-
+# define s3 resource using boto3 
+s3_resource = boto3.resource('s3')
 s3 = boto3.resource('s3')
 
-directory_in_str="photos"
+# load configs
+with open("config/s3_var.yaml", "r") as config:
+	try:
+		config = yaml.safe_load(config)
+		print("\nMy Config = {}\n".format(config))
+	except yaml.YAMLError as exc:
+		print(exc)
 
-directory = os.fsencode(directory_in_str)
+# varaibles
+from_local_dir = config['local']['local_dir']
+s3_bucket_name = config['upload']['s3_bucket_name']
+photo_dir = config['upload']['s3_dir']['photo_dir']
+video_dir = config['upload']['s3_dir']['video_dir']
+music_dir = config['upload']['s3_dir']['music_dir']
+documents_dir = config['upload']['s3_dir']['documents_dir']
 
-for file in os.listdir(directory):
-    filename = os.fsdecode(file)
-    if str.upper(filename).endswith(".JPEG") or str.upper(filename).endswith(".JPG") or str.upper(filename).endswith(".PNG") or str.upper(filename).endswith(".HEIC"):
-        strg=directory_in_str+'/'+filename
-        print(strg)     
-        file = open(strg,'rb')
-        object = s3.Object('factspan-mac-backup','Photos/2021/November/'+filename)
-        object.put(Body=file,ContentType='image/jpeg')
+# print the content
+print("\nMy Local directory = {}\n".format(from_local_dir))
+print("\nMy s3 bucket name = {}\n".format(s3_bucket_name))
+print("\nMy s3 photo directory = {}\n".format(photo_dir))
+print("\nMy s3 video directory = {}\n".format(video_dir))
+print("\nMy s3 music directory = {}\n".format(music_dir))
+print("\nMy s3 documents directory = {}\n".format(documents_dir))
 
-    if str.upper(filename).endswith(".MOV"):
-        strg=directory_in_str+'/'+filename
-        print(strg)     
-        file = open(strg,'rb')
-        object = s3.Object('factspan-mac-backup','Videos/2021/November/'+filename)
-        object.put(Body=file,ContentType='video/mov')
+directory = os.fsencode(from_local_dir)
 
-    if str.upper(filename).endswith(".MP4"):
-        strg=directory_in_str+'/'+filename
-        print(strg)     
-        file = open(strg,'rb')
-        object = s3.Object('factspan-mac-backup','Videos/2021/November/'+filename)
-        object.put(Body=file,ContentType='video/mp4')
+# main program
+def main():
+	print("--------->> Inside main method")
+	for file in os.listdir(directory):
+		filename = os.fsdecode(file)
+		if str.upper(filename).endswith(".JPEG") or str.upper(filename).endswith(".JPG") or str.upper(filename).endswith(".PNG") or str.upper(filename).endswith(".HEIC"):
+			upload_to_s3(filename, photo_dir, 'image/jpeg')
 
-    if str.upper(filename).endswith(".MP3"):
-        strg=directory_in_str+'/'+filename
-        print(strg)     
-        file = open(strg,'rb')
-        object = s3.Object('factspan-mac-backup','Music/2021/November/'+filename)
-        object.put(Body=file,ContentType='audio/mp3')
+		if str.upper(filename).endswith(".MOV"):
+			upload_to_s3(filename, video_dir, 'video/mov')
 
-    else:
-        continue
+		if str.upper(filename).endswith(".MP4"):
+			upload_to_s3(filename, video_dir, 'video/mp4')
+
+		if str.upper(filename).endswith(".MP3"):
+			upload_to_s3(filename, music_dir, 'audio/mp3')
+
+		else:
+			continue
+
+
+# user defined functions
+def upload_to_s3(filename, to_directory, type):
+	print("file = {} is moving to s3 dir = {} of type = {}".format(filename, to_directory, type))
+	strg = from_local_dir + '/' + filename
+	file = open(strg,'rb')
+	object = s3.Object(s3_bucket_name, to_directory + filename)
+	object.put(Body=file, ContentType = type)
+
+
+# run main
+if __name__ == "__main__":
+	main()
